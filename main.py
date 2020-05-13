4ea5c7e78e95c8c98753256c6b134716640ec4c6
Shubham Juneja <shubhamjuneja9@gmail.com>
2020-05-13 22:15:01 +0300

    Adds weights loading, removes unwanted code

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def check_total_params(network):
         total += current
     print(total)
 
-
 # Model Definition
 class Net(nn.Module):
     def __init__(self):
@@ -93,22 +92,9 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
-# Print model
 net = Net()
-print(net)
 
 # Notes:
-
-# ['model_weights']
-#['block1_conv1', 'block1_conv2', 'block1_pool', 
-# 'block2_conv1', 'block2_conv2', 'block2_pool', 
-# 'block3_conv1', 'block3_conv2', 'block3_conv3', 
-# 'block3_pool', 'block4_conv1', 'block4_conv2', 
-# 'block4_conv3', 'block4_pool', 'block5_conv1', 
-# 'block5_conv2', 'block5_conv3', 'block5_pool', 
-# 'dense', 'flatten', 'input_1']
-
-# f['/model_weights/block1_conv1/']['block1_conv1'].keys()
 
 # given format: 3,3,3,64
 # torch format: 64,3,3,3
@@ -119,45 +105,28 @@ print(net)
 
 # convert kernelxkernel , incoming, outgoing TO outgoing, incoming, kernelxkernel
 
-def transpose(h5_tensor):
+def make_param(pytorch_tensor, bias=False):
     # NOTE: Experimental, one should work
-    pytorch_tensor = h5_tensor.transpose((3,2,0,1))
-    # pytorch_tensor = h5_tensor.transpose((3,2,1,0))
+    if not bias:
+        pytorch_tensor = pytorch_tensor.transpose((3,2,0,1))
+        # pytorch_tensor = h5_tensor.transpose((3,2,1,0))
 
     # Convert to parameter
-    pytorch_tensor = torch.nn.Parameter(pytorch_tensor)
+    pytorch_tensor = torch.nn.Parameter(torch.from_numpy(pytorch_tensor))
     return pytorch_tensor
 
+# Loading weights to model
 f = h5py.File('/home/shubham/Downloads/tradeoff_network_vgg3_case10_03-0.98.hdf5')
+blocked_layers = [[net.block1_conv1, net.block1_conv2], [net.block2_conv1, net.block2_conv2],[net.block3_conv1, net.block3_conv2, net.block3_conv3], [net.block4_conv1,
+                 net.block4_conv2, net.block4_conv3], [net.block5_conv1, net.block5_conv2, 
+                 net.block5_conv3]]
 
-print(net.block2_conv1.bias.shape)
-print(net.block2_conv1.weight.shape)
-print(f['/model_weights/block2_conv1/']['block2_conv1']['kernel:0'][:].shape)
+for block_number, block in enumerate(blocked_layers):
+    block_number += 1
+    for layer_number, layer in enumerate(block):
+        layer_number += 1
+        address = "block{}_conv{}".format(block_number, layer_number)
+        print(layer)
+        layer.weight = make_param(f['/model_weights/{}/'.format(address)][address]['kernel:0'][:])
+        layer.bias = make_param(f['/model_weights/{}/'.format(address)][address]['bias:0'][:], True)
 
-
-# NOTE: Better way to do this would be state dictionary, but meh
-# Loading weights:
-
-net.block1_conv1.weight = transpose(f['/model_weights/block1_conv1/']['block1_conv1']['kernel:0'][:])
-net.block1_conv1.bias = transpose(f['/model_weights/block1_conv1/']['block1_conv1']['bias:0'][:])
-
-net.block1_conv2 = nn.Conv2d(64, 64, 3, 1)
-
-# Block 2
-net.block2_conv1 = nn.Conv2d(64, 128, 3, 1)
-net.block2_conv2 = nn.Conv2d(128, 128, 3, 1)
-
-# Block 3
-self.block3_conv1 = nn.Conv2d(128, 256, 3, 1)
-self.block3_conv2 = nn.Conv2d(256, 256, 3, 1)
-self.block3_conv3 = nn.Conv2d(256, 256, 3, 1)
-
-# Block 4
-self.block4_conv1 = nn.Conv2d(256, 512, 3, 1)
-self.block4_conv2 = nn.Conv2d(512, 512, 3, 1)
-self.block4_conv3 = nn.Conv2d(512, 512, 3, 1)
-
-# Block 5
-self.block5_conv1 = nn.Conv2d(512, 512, 3, 1)
-self.block5_conv2 = nn.Conv2d(512, 512, 3, 1)
-self.block5_conv3 = nn.Conv2d(512, 512, 3, 1)
